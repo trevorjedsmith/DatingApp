846ffc5681dfe9285b87f736c6ba59da6540b7e7
trevor smith <trevor_jed_smith@hotmail.com>
2019-01-14 15:34:00 +1030

    Security and Jwt setup complete

--- DatingApp.Api/Data/DataContext.cs
+++ DatingApp.Api/Data/DataContext.cs
@@ -11,6 +11,7 @@ namespace DatingApp.Api.Data
         public DataContext(DbContextOptions<DataContext> options):base(options)
         { }
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 
     public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
